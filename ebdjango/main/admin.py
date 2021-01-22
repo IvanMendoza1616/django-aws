b2f7a9451157d82155bb64800180039205c84734
IvanMendoza1616 <navi96@hotmail.es>
2021-01-22 12:59:38 -0600

    Added section to ingredients

--- ebdjango/main/admin.py
+++ ebdjango/main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import ToDoList, Item
 from .models import Recipe, Ingredient, Preparation
+
 # Register your models here.
 admin.site.register(ToDoList)
 admin.site.register(Item)
--- ebdjango/main/views.py
+++ ebdjango/main/views.py
@@ -16,6 +16,7 @@ def index(response, id):
 			for ingredient in ls.ingredient_set.all():
 				ingredient.quantity = response.POST.get("q" + str(ingredient.id))
 				ingredient.text = response.POST.get("t" + str(ingredient.id))
+				ingredient.section = response.POST.get("s" + str(ingredient.id))
 				ingredient.save()
 			for preparation in ls.preparation_set.all():
 				preparation.preparation = response.POST.get(str(preparation.id))
@@ -81,21 +82,28 @@ def recipes(response):
 def shoppinglist(response):
 	sl = []
 	ql = []
+	temp = []
 	shoppinglist = ""
 	rl = Recipe.objects.order_by('name').exclude(quantity=0)
 	for recipe in rl:
 		for ingredient in recipe.ingredient_set.all():
 			if ingredient.text not in sl:
-				sl.append(ingredient.text)
+				sl.append(ingredient.section + "#" + ingredient.text)
 				ql.append(ingredient.quantity*recipe.quantity)
 			else:
 				ql[sl.index(ingredient.text)] += ingredient.quantity
 
 	for i in range(len(sl)):
 		if (ql[i] % 1 == 0):
-			shoppinglist += str(sl[i]) + " x " + str(int(ql[i])) + "\n"
+			temp.append(str(sl[i]) + " x " + str(int(ql[i])) + "\n")
 		else:
-			shoppinglist += str(sl[i]) + " x " + str(round(ql[i],1)) + "\n"
+			temp.append(str(sl[i]) + " x " + str(round(ql[i],1)) + "\n")
+
+	temp = sorted(temp)
+
+	for i in range(len(temp)):
+		temp1 = temp[i].split("#")
+		shoppinglist += temp1[1]
 
 
 	return render(response, "main/shoppinglist.html", {"shoppinglist":shoppinglist})
